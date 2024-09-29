6ad1815184aedaaab12710abf60dd43fe6d45c89
hustle2live <kuz.v@ukr.net>
2024-09-29 23:48:28 +0300

    fix: changed dependencies

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -7,15 +6,8 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	root: './',
-	build: {
-		outDir: './dist',
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-				nested: resolve(__dirname, 'index.html'),
-			},
-		},
-	},
 	plugins: [react(), svgr(), ViteAliases(), EnvironmentPlugin('all')],
+	build: {
+		outDir: 'dist',
+	},
 });
