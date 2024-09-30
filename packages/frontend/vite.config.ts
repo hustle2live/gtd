1648df7b9d2a2d9f3a1061abedc6837ac0caab9d
hustle2live <kuz.v@ukr.net>
2024-09-30 18:13:15 +0300

    adding dist

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/gtd/',
 	plugins: [
 		react(),
 		svgr(),
@@ -14,5 +15,4 @@ export default defineConfig({
 		EnvironmentPlugin('all'),
 		tsconfigPaths(),
 	],
-	base: '/gtd/',
 });
