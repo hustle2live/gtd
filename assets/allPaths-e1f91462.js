5f2416d795a781cd6d61ea508b97f6a2c3247315
hustle2live <kuz.v@ukr.net>
2025-03-06 23:02:09 +0200

    Updates

--- assets/allPaths-e1f91462.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-92cb7726.js";import{I as e}from"./index-6e80c5dc.js";import{p as r,I as s}from"./index-0449e275.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
